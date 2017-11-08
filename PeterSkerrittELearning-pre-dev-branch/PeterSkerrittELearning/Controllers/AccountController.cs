666237c18f4c859242e804dfb16c64afd2b773c8
Leighshe <leighshebashing1@gmail.com>
2017-11-08 10:51:06 +0200

    Fix Code

--- PeterSkerrittELearning-pre-dev-branch/PeterSkerrittELearning/Controllers/AccountController.cs
+++ PeterSkerrittELearning-pre-dev-branch/PeterSkerrittELearning/Controllers/AccountController.cs
@@ -14,7 +14,7 @@ using PeterSkerrittELearning.Entities;
 namespace PeterSkerrittELearning.Controllers
 {
     [Authorize]
-   
+    public class AccountController : Controller
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
